c4cd02edbaf8d599b2683e19137551ebf68e27b8
Senku01 <vigneshprakash2003@gmail.com>
2023-02-06 23:39:58 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState,useEffect } from 'react';
+import { ThemeProvider } from './context/ThemeContext';
+import axios from 'axios';
+import Nav from './components/Nav'
+import Home from './routes/Home'
+import Signin from './routes/Signin'
+import Signup from './routes/Signup'
+import Account from './routes/Account'
+import CoinPage from './routes/CoinPage'
+import { BrowserRouter as Router,Route } from 'react-router-dom'
+import Footer from './components/Footer';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [coins,setCoins]= useState([])
+
+  const url ='https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=true'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      // console.log(response.data)
+    })
+  },[url])
+
+
+
+  return <ThemeProvider>
+    <Nav />
+    <Router>
+      <Route exact path='/'>
+        <Home coins={coins}/>
+      </Route>
+      <Route exact path='/signin'>
+        <Signin />
+      </Route>
+      <Route exact path='/signup'>
+        <Signup />
+      </Route>
+      <Route exact path='/account'>
+        <Account />
+      </Route>
+      <Route path='/coin/:coinId'>
+        <CoinPage /> 
+        <Route path=':coinId' />
+      </Route>
+    </Router>
+    <Footer />
+  </ThemeProvider>
 }
 
 export default App;
